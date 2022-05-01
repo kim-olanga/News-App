bae48e62ee116b25e1163533d05e34022ce1223d
Kimberly <kimolanga123@gmail.com>
2022-05-01 17:08:02 +0300

    add test file

--- app/config.py
+++ app/config.py
@@ -1,11 +1,13 @@
 from distutils.debug import DEBUG
 
+from instance.config import NEWS_API_KEY
+
 
 class Config:
     """
     General parent configuration class
     """
-    pass
+    NEWS_API_BASE_URL = 'https://https://newsapi.org/news/{}?api_key={}'
 
 class ProdConfig(Config):
     """
