090ba6829254a335b97d24629ee504e08fdb2691
Kimberly <kimolanga123@gmail.com>
2022-05-01 14:46:35 +0300

    add class config && class prodconfig

---
+++ app/config.py
@@ -0,0 +1,13 @@
+class Config:
+    """
+    General parent configuration class
+    """
+    pass
+
+class ProdConfig(Config):
+    """
+    Production configuration child class
+    Args:
+        config: The parent configuration class with General configuration settings
+    """
+    pass
--- app/views.py
+++ app/views.py
@@ -11,13 +11,6 @@ def index():
     message = 'Hello News'
     return render_template('index.html', message = message)
 
-@app.route('/news/<news_id>')
-def news(news_id):
-    """
-    View news page function that returns the news' details page and its data
-    """
-    return render_template('news.html',id = news_id)
-
 @app.route('/news/<int:news_id>')
 def news(news_id):
     """
