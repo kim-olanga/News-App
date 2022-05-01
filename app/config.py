d8163a0e61a518d5c97fc4d8e55302a467612bae
Kimberly <kimolanga123@gmail.com>
2022-05-01 14:49:48 +0300

    add class devconfig

--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
+from distutils.debug import DEBUG
+
+
 class Config:
     """
     General parent configuration class
@@ -11,3 +14,12 @@ class ProdConfig(Config):
         config: The parent configuration class with General configuration settings
     """
     pass
+
+class DevConfig(Config):
+    """
+    Development configuration child class
+    Args: 
+        Config: The parent configuration  class with General configuration settings
+    """
+
+    # DEBUG = True
